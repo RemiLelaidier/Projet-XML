d698b026bc14a3b6511cfc181d3abe4a4eb9c496
Rlelaidier <r.lelaidier@hotmail.fr>
2017-12-07 16:43:35 +0100

    Database skeleton

---
+++ serv/src/main/java/unice/miage/xml/database/Database.java
@@ -0,0 +1,26 @@
+package unice.miage.xml.database;
+
+import org.inria.fr.ns.cr.*;
+import org.inria.fr.ns.sr.*;
+
+public class Database {
+    private String xmlPath = "";
+    private String xsdPath = "";
+
+    public Database(){
+
+    }
+
+    public Crs getCentersList(){
+        // Get Centers List
+        return new Crs();
+    }
+
+    private void connect(){
+        // Connect
+    }
+
+    private void disconnect(){
+        // Disconnect
+    }
+}
