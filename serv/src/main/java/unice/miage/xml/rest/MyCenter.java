7fc16d46407ee788ebd8990e2ec56214bf88c418
Rlelaidier <r.lelaidier@hotmail.fr>
2017-12-07 11:16:07 +0100

    API /centers/list return list

--- serv/src/main/java/unice/miage/xml/rest/MyCenter.java
+++ serv/src/main/java/unice/miage/xml/rest/MyCenter.java
@@ -7,6 +7,8 @@ import javax.ws.rs.core.MediaType;
 
 import unice.miage.xml.model.Center;
 
+import java.util.ArrayList;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
@@ -22,12 +24,23 @@ public class MyCenter {
     @GET
     @Path("list")
     @Produces(MediaType.APPLICATION_JSON)
-    public Center getCenterList() {
+    public ArrayList<Center> getCenterList() {
+        ArrayList<Center> centers = new ArrayList<Center>();
+
         Center c1 = new Center();
         c1.setId(1);
         c1.setName("I3S");
         c1.setLongitude("43.6156381");
         c1.setLatitude("7.0668937");
-        return c1;
+
+        Center c2 = new Center();
+        c2.setId(2);
+        c2.setName("Resus");
+        c2.setLongitude("43.6156400");
+        c2.setLatitude("7.0668901");
+        centers.add(c1);
+        centers.add(c2);
+
+        return centers;
     }
 }
