ff6a1b6cd4f2724e0d7982c9e594e160aac06766
etienne <etienne.laskar74@gmail.com>
2018-01-24 18:42:16 +0100

    fix bug

--- serv/src/main/java/unice/miage/xml/builder/XQueryBuilder.java
+++ serv/src/main/java/unice/miage/xml/builder/XQueryBuilder.java
@@ -43,4 +43,12 @@ public class XQueryBuilder {
          return result; 
     	
     }
+
+    public ArrayList getTeamsClose() {
+        return null;
+    }
+
+    public ArrayList getTeamsOpen() {
+        return null;
+    }
 }
