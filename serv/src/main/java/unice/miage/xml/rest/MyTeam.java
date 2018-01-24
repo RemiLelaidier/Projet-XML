a8e323578aef0fd9a80186d3703696714c97a826
etienne <etienne.laskar74@gmail.com>
2018-01-24 18:26:56 +0100

    MyTeam can send open and close teams

--- serv/src/main/java/unice/miage/xml/rest/MyTeam.java
+++ serv/src/main/java/unice/miage/xml/rest/MyTeam.java
@@ -1,5 +1,7 @@
 package unice.miage.xml.rest;
 
+import unice.miage.xml.builder.XQueryBuilder;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,13 +15,17 @@ public class MyTeam {
     @Path("/list/open")
     @Produces(MediaType.APPLICATION_JSON)
     public ArrayList getOpen(){
-        return null;
+        XQueryBuilder xQueryBuilder = new XQueryBuilder();
+        ArrayList result = xQueryBuilder.getTeamsOpen();
+        return result;
     }
 
     @GET
     @Path("/list/close")
     @Produces(MediaType.APPLICATION_JSON)
     public ArrayList getClose(){
-        return null;
+        XQueryBuilder xQueryBuilder = new XQueryBuilder();
+        ArrayList result = xQueryBuilder.getTeamsClose();
+        return result;
     }
 }
